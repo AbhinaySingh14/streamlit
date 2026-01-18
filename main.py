2efb7ac035d99b5f12ec12c17ef84f6721d805bf
Abhinay Singh <singhabhi0914@gmail.com>
2026-01-18 19:03:44 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('First Streamlit App')
+
+st.subheader('Introducing My first webApp')
+st.write(''' welcome everyone this is my first web app
+         enjoy it!''')
+
+#run by typing (streamlit run main.py) in terminal
+#to deploy it connect to fithub using version controll and push it
+# then go to 
