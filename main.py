f1d68537943b643969208751dad7b156b4a0c5e6
Abhinay Singh <singhabhi0914@gmail.com>
2026-01-18 20:49:39 +0530

    added table

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import streamlit as st
-
+import pandas as pd
+d={'name':['abhi','sunil','naman'],'marks':[100,77,87]}
+df=pd.DataFrame(d)
 st.title('First Streamlit App')
 
 st.subheader('Introducing My first webApp')
 st.write(''' welcome everyone this is my first web app
          enjoy it!''')
+st.write(df)
 
 #run by typing (streamlit run main.py) in terminal
 #to deploy it connect to fithub using version controll and push it
