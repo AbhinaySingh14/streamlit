d251480b2acd9b8cb774d2661c17eb859496b010
Abhinay Singh <singhabhi0914@gmail.com>
2026-01-18 20:55:56 +0530

    added graph

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ st.subheader('Introducing My first webApp')
 st.write(''' welcome everyone this is my first web app
          enjoy it!''')
 st.write(df)
+st.line_chart(df)
 
 #run by typing (streamlit run main.py) in terminal
 #to deploy it connect to fithub using version controll and push it
